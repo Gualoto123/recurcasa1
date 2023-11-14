212e075491adcec13c7990571ed501333248bad9
Edwin11 <edwin.gualoto.quilumba@udla.edu.ec>
2023-11-14 10:42:39 -0500

    ejerrecur.

---
+++ src/Principall.java
@@ -0,0 +1,6 @@
+public class Principall {
+    public static void main(String[] args) {
+        Recursividad r= new Recursividad();
+        r.Imprimir(1);
+    }
+}
---
+++ src/Recursividad.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Recursividad {
+
+    public void Imprimir (int x){
+        if(x <=5 ){
+
+            System.out.println(x+ " ");
+            Imprimir(x+1);
+
+        }
+    }
+
+}
